79a35eb6faf6fed697f0d20a22df73a0fba351a2
allison-truhlar <allison-truhlar@users.noreply.github.com>
2024-04-18 17:29:05 +0000

    Deploying to gh-pages from @ allison-truhlar/ossi-website-action-tests@afbe98dfbbcb00757fed2b19c1ab0bb7ea5a67ad 🚀

---
+++ _astro/getRandomImage.ClaK_-b2.js
@@ -0,0 +1,9 @@
+import{r as l}from"./index.D3g0PtM7.js";var _={exports:{}},n={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var m=l,u=Symbol.for("react.element"),i=Symbol.for("react.fragment"),d=Object.prototype.hasOwnProperty,x=m.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function a(t,r,f){var e,o={},s=null,p=null;f!==void 0&&(s=""+f),r.key!==void 0&&(s=""+r.key),r.ref!==void 0&&(p=r.ref);for(e in r)d.call(r,e)&&!c.hasOwnProperty(e)&&(o[e]=r[e]);if(t&&t.defaultProps)for(e in r=t.defaultProps,r)o[e]===void 0&&(o[e]=r[e]);return{$$typeof:u,type:t,key:s,ref:p,props:o,_owner:x.current}}n.Fragment=i;n.jsx=a;n.jsxs=a;_.exports=n;var v=_.exports;function R(t){const r=Math.floor(Math.random()*t.length);return t[r]}export{R as g,v as j};
